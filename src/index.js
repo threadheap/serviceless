0a65eda418107e466b93d0bdbf0478ff7e2b50f3
Pavel Vlasov <pvlasov@atlassian.com>
2018-01-03 17:12:40 +1100

    prepare for prod

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ program
         '-b, --runInBand',
         'Deploy services one by one (parallel by default)'
     )
-    .description('deploy service')
+    .description('deploy service (all to deploy the whole directory)')
     .action((service, options) => {
         const args = program.rawArgs.slice(service ? 4 : 3);
 
@@ -26,6 +26,10 @@ program
             options
         );
     });
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
 
 program.parse(process.argv);
 
